13c4b1bfe5986f2e504116d1f4403768d97faee0
Alec Lima <aleclimadev@gmail.com>
2023-04-06 16:19:45 -0300

    chore: section grid

---
+++ src/Clothing/Section.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Sections = styled.section`
+  width: 95%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  gap: 2rem;
+  padding-bottom: 6rem;
+`;
+
+export const Main = styled.main`
+  margin-top: 15vh;
+  width: 100%;
+  min-height: 60vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #f8f7f7;
+  z-index: 0;
+`;
