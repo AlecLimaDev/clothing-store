2460551add26b7e15aa4d8ac07b39e567446d1f9
Alec Lima <aleclimadev@gmail.com>
2023-04-07 14:38:14 -0300

    feat: api fake with items of store

---
+++ database/index.ts
@@ -0,0 +1,7 @@
+export const clothing = [
+    { url: "/roupaFeminina.svg", name: "Peça Feminina", price: 50 },
+    { url: "/shirtBlue.svg", name: "Camiseta Azul", price: 40 },
+    { url: "/shirtWhite.svg", name: "Camiseta Branca", price: 40 },
+    { url: "/sueterPreto.svg", name: "Sueter Preto", price: 70 },
+    { url: "/sueterRosa.svg", name: "Sueter Rosa", price: 73.99 }
+]
