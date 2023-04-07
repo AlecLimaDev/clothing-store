54aa37a951fb963ea1eb58cedb42ebbd096f2dbf
Alec Lima <aleclimadev@gmail.com>
2023-04-07 15:00:53 -0300

    Chore: section and main  with column at center

---
+++ src/components/Clothing/Section.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+
+export const Main = styled.main`
+  margin-top: 15vh;
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+`;
+
+
+
+export const Sections = styled.section`
+  width: 80%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 4rem;
+  align-items: center;
+  padding-bottom: 1rem;
+  text-align: center;
+`;
+
+
