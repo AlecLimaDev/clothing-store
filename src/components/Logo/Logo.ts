a6c03dfe2412714f130b506f4a34ee67d758d34a
Alec Lima <aleclimadev@gmail.com>
2023-04-07 14:59:21 -0300

    Chore: Title config and addition responsive

---
+++ src/components/Logo/Logo.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Logo = styled.h2`
+  font-size: 2rem;
+  font-weight: 500;
+  font-family: sans-serif;
+
+
+  .logo-bold {
+  font-weight: bold;
+  font-family: sans-serif;
+  color: antiquewhite;
+}
+
+@media (max-width: 520px) {
+  h2 {
+    font-size: 1rem;
+    
+  }
+}
+`;
